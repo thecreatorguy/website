dc967bfdddc87667ce300acd914f28c8db58a230
root <root@DESKTOP-TT9T2TT.localdomain>
2019-09-28 22:49:51 -0400

    starting new website frontend

--- application/views/home2.php
+++ application/views/home2.php
@@ -5,16 +5,22 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>It's Tim Johnson</title>
-    <link rel="shortcut icon" href="assets/images/favicon.ico">
     <link rel="stylesheet" type="text/css" href="assets/css/website.css">
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
 </head>
 
 <body>
+    <header>
+        <?php echo anchor('/', 'hey, it\'s Tim Johnson', 'id="title"'); ?>
+        <nav>
+            <ul>
+                <li class="v-center-container"><?php echo anchor('/', 'About'); ?></li>
+                <li class="v-center-container"><?php echo anchor('/', 'Projects'); ?></li>
+                <li class="v-center-container"><?php echo anchor('/', 'Contact&nbsp;Me'); ?></li>
+            </ul>
+        </nav>
+    </header>
     <main>
-        <header>
-            <h1>Tim Johnson's Website</h1>
-            <p>Hello, and welcome to my website! This is a temporary version of it while I work on the much cooler looking version.</p>
-        </header>
         
     </main>
 </body>
