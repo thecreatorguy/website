6ef3d2b6f1c8a10ddbd67e0a295e6d8aa01bca9d
root <root@DESKTOP-TT9T2TT.localdomain>
2019-10-07 22:31:20 -0400

    added new content to home page

--- application/views/pages.php
+++ application/views/pages.php
@@ -14,7 +14,7 @@
         <?php echo anchor('/', 'hey,&nbsp;it\'s&nbsp;Tim&nbsp;Johnson', 'id="title"'); ?>
         <nav>
             <ul>
-                <li class="v-center-container"><?php echo anchor('/', 'About', 'class="current"'); ?></li>
+                <li class="v-center-container"><?php echo anchor('/', 'About&nbsp;Me', 'class="current"'); ?></li>
                 <li class="v-center-container"><?php echo anchor('/blog', 'Blog'); ?></li>
                 <li class="v-center-container"><?php echo anchor('/projects', 'Projects'); ?></li>
                 <li class="v-center-container"><?php echo anchor('/contact', 'Contact&nbsp;Me'); ?></li>
@@ -22,21 +22,12 @@
         </nav>
     </header>
     <div id="content-container">
-        <main>
-            <img src="assets/images/home-logo.svg" alt="logo" width="650px"/>
-            <div id="text">
-                <h1>hello it is i, Tim Johnson. if you want to know more about me, i've got you covered right here</h1>
-                <h2>what am i?</h2>
-                <p>
-                    i'm a software engineer, an anime enthusiast, and a loving daughter. catch i'll catch ya
-                    on the flip side, nerds
-                </p>
-                <h2>who am i?</h2>
-                <p>i just told you i'm tim johnson. can you read?</p>
-                <h2>where am i?</h2>
-                <p>help?</p>
-            </div>
-        </main>
+        <div id="content-column">
+            <div id="header-spacer"></div>
+            <main>
+                <img src="assets/images/logo2.svg" alt="logo" height="450px"/>
+            </main>
+        </div>
     </div>
 </body>
 
