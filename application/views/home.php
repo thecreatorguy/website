b0dd8569094a434b810c0cde3761e4d4251679b1
root <root@DESKTOP-TT9T2TT.localdomain>
2019-10-09 21:00:36 -0400

    a bit more content

--- application/views/home.php
+++ application/views/home.php
@@ -14,7 +14,7 @@
         <?php echo anchor('/', 'hey,&nbsp;it\'s&nbsp;Tim&nbsp;Johnson', 'id="title"'); ?>
         <nav>
             <ul>
-                <li class="v-center-container"><?php echo anchor('/', 'About', 'class="current"'); ?></li>
+                <li class="v-center-container"><?php echo anchor('/about', 'About'); ?></li>
                 <li class="v-center-container"><?php echo anchor('/blog', 'Blog'); ?></li>
                 <li class="v-center-container"><?php echo anchor('/projects', 'Projects'); ?></li>
                 <li class="v-center-container"><?php echo anchor('/contact', 'Contact&nbsp;Me'); ?></li>
@@ -25,14 +25,18 @@
         <main>
             <img src="assets/images/home-logo.svg" alt="logo" width="650px"/>
             <div id="text">
-                <h1>hello it is i, Tim Johnson. if you want to know more about me, i've got you covered right here</h1>
-                <h2>what am i?</h2>
+                <h1>Hey, I'm Tim Johnson. Here's a summary of myself:</h1>
+                <h2>Who am I?</h2>
+                <p>
+                    If I (Tim Johnson if you didn't catch that by now) were to choose three descriptions for myself,
+                    I would pick software engineer/math guy, someone who watches anime and cartoons more often than 
+                    live-action television, and well-adjusted adult.
+                </p>
+                <h2>What am i?</h2>
                 <p>
                     i'm a software engineer, an anime enthusiast, and a loving daughter. catch i'll catch ya
                     on the flip side, nerds
                 </p>
-                <h2>who am i?</h2>
-                <p>i just told you i'm tim johnson. can you read?</p>
                 <h2>where am i?</h2>
                 <p>help?</p>
             </div>
