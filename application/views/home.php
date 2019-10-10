b258904b485123366b5a92ab9a68ffdd9f678ed0
root <root@DESKTOP-TT9T2TT.localdomain>
2019-10-09 21:55:52 -0400

    finished home page content

--- application/views/home.php
+++ application/views/home.php
@@ -14,7 +14,7 @@
         <?php echo anchor('/', 'hey,&nbsp;it\'s&nbsp;Tim&nbsp;Johnson', 'id="title"'); ?>
         <nav>
             <ul>
-                <li class="v-center-container"><?php echo anchor('/about', 'About'); ?></li>
+                <li class="v-center-container"><?php echo anchor('/about', 'About', 'class="current"'); ?></li>
                 <li class="v-center-container"><?php echo anchor('/blog', 'Blog'); ?></li>
                 <li class="v-center-container"><?php echo anchor('/projects', 'Projects'); ?></li>
                 <li class="v-center-container"><?php echo anchor('/contact', 'Contact&nbsp;Me'); ?></li>
@@ -25,20 +25,32 @@
         <main>
             <img src="assets/images/home-logo.svg" alt="logo" width="650px"/>
             <div id="text">
-                <h1>Hey, I'm Tim Johnson. Here's a summary of myself:</h1>
+                <h1>Hi, I'm Tim Johnson. Here's a quick summary of myself:</h1>
                 <h2>Who am I?</h2>
                 <p>
                     If I (Tim Johnson if you didn't catch that by now) were to choose three descriptions for myself,
                     I would pick software engineer/math guy, someone who watches anime and cartoons more often than 
-                    live-action television, and well-adjusted adult.
+                    live-action television, and well-adjusted adult. Some day, you might be able to find more out about
+                    me at my <?php echo anchor('/blog', 'blog') ?>.
                 </p>
-                <h2>What am i?</h2>
+                <h2>What do I do?</h2>
                 <p>
-                    i'm a software engineer, an anime enthusiast, and a loving daughter. catch i'll catch ya
-                    on the flip side, nerds
+                    Currently my projects are few, but the gears are turning. You can find them on my 
+                    <?php echo anchor('/projects', 'projects') ?> page. In my personal free time, I like to play D&D 
+                    (if my friends could ever find the time), meditate, and maintain a blissful ignorance of pop
+                    culture and most social media platforms. 
                 </p>
-                <h2>where am i?</h2>
-                <p>help?</p>
+                <h2>What do I want?</h2>
+                <p>
+                    Professionally, I want to gain a broad range of experience in many different areas of computer
+                    science. As a student halfway through college, the exposure to different topics will hopefully give
+                    me a stronger idea of what things I enjoy doing and what I do not. So far, I have discovered that
+                    I want my future career to involve more math than my current internship using web technologies.
+                </p>
+                <p>
+                    As for my personal goals, I'm currently in pursuit of self improvement. Among my goals are to
+                    branch out socially, become the best physical version of myself, and explore creative activities.
+                </p>
             </div>
         </main>
     </div>
