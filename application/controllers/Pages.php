57d1ad66c0793a355e497f25a9217a45bd9764f9
root <root@DESKTOP-TT9T2TT.localdomain>
2019-10-12 20:02:43 -0400

    finished social media, broke up phtml templates to be reused

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,16 +1,24 @@
 <?php
 
+/**
+ * Controller for rendering any page for the website
+ *
+ * @author  Tim Johnson <tim@itstimjohnson.com>
+ */
 class Pages extends CI_Controller
 {
-    const PAGES = [
-        'home',
-        'blog',
-        'projects',
-        'contact',
-        'slider',
-        'resume',
-        'home_old',
+    // Map of pages (/page) to the name/title
+    const PAGE_NAMES = [
+        'home'     => 'Home',
+        'blog'     => 'Blog',
+        'projects' => 'Projects',
+        'contact'  => 'Contact Me',
+        'slider'   => 'Slider Game',
+        'resume'   => 'Resume',
     ];
+
+    // Path to where partial templates are located
+    const PARTIAL_PATH = __DIR__ . '/../views/partials';
 
     /**
      * Create the index of the website, loading the home page
@@ -19,21 +27,46 @@ class Pages extends CI_Controller
      */
     public function index()
     {
-        $this->view('home');
+        $data = [
+            'head'   => $this->partial('head', ['page_name' => 'Home', 'css_file' => 'home'], true),
+            'header' => $this->partial('header'),
+            'footer' => $this->partial('footer'),
+        ];
+
+        $this->load->view('home.phtml', $data);
     }
 
     /**
      * View the given page
      *
-     * @param  string $page Title of the view to load
+     * @param  string $page  Title of the view to load
      * @return void
      */
     public function view($page)
     {
-        if (!in_array($page, self::PAGES)) {
+        if (!in_array($page, array_keys(self::PAGE_NAMES))) {
             show_404();
         }
-        $this->load->helper('url');
-        $this->load->view($page);
+
+        $data = [
+            'title'  => self::PAGE_NAMES[$page],
+            'head'   => $this->partial('head', ['page_name' => self::PAGE_NAMES[$page], 'css_file' => 'page']),
+            'header' => $this->partial('header'),
+            'footer' => $this->partial('footer'),
+            'page'   => $this->partial('pages/' . $page)
+        ];
+        $this->load->view('page.phtml', $data);
+    }
+
+    /**
+     * Get a fully rendered part of a view to be inserted into another view
+     *
+     * @param  string  $name  Name of the partial view to load, without extension
+     * @param  array   $data  Data to render the partial view with
+     * @return string         Fully rendered html
+     */
+    private function partial($name, $data = [])
+    {
+        return $this->load->view("partials/${name}.phtml", $data, true);
     }
 }
