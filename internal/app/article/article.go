ea9a48bd10e7034b619f8f321217059827542b59
Tim Johnson <tim@itstimjohnson.com>
2020-11-06 17:56:56 -0500

    added in articles

--- internal/app/article/article.go
+++ internal/app/article/article.go
@@ -1,1 +1,47 @@
 package article
+
+import (
+	"database/sql"
+	"website/internal/sqlconn"
+)
+
+type Article struct {
+	URLKey string `json:"url_key"`
+	Title string `json:"title"`
+	Content string `json:"content"`
+	ReleaseAt sql.NullTime `json:"release_at"`
+	CreatedAt sql.NullTime `json:"created_at"`
+	UpdatedAt sql.NullTime `json:"updated_at"`
+}
+
+func GetArticle(key string) (Article, error) {
+	row := sqlconn.Pool.QueryRow("SELECT * FROM articles WHERE url_key = $1", key)
+
+	a := Article{}
+	err := row.Scan(&a.URLKey, &a.Title, &a.Content, &a.ReleaseAt, &a.CreatedAt, &a.UpdatedAt)
+	if err != nil {
+		return a, err
+	}
+
+	return a, nil
+}
+
+func GetArticles() ([]Article, error) {
+	rows, err := sqlconn.Pool.Query("SELECT * FROM articles ORDER BY release_at DESC")
+	if err != nil {
+		return nil, err
+	}
+
+	as := []Article{}
+	for rows.Next() {
+		var a Article
+		err := rows.Scan(&a.URLKey, &a.Title, &a.Content, &a.ReleaseAt, &a.CreatedAt, &a.UpdatedAt)
+		if err != nil {
+			return nil, err
+		}
+
+		as = append(as, a)
+	}
+	
+	return as, nil
+}
--- internal/sqlconn/sqlconn.go
+++ internal/sqlconn/sqlconn.go
@@ -1,9 +1,32 @@
 package sqlconn
 
-import "github.com/jackc/pgx"
+import (
+	"context"
+	"os"
 
-var Pool pgx.ConnPool
+	"github.com/jackc/pgx"
+)
+
+var Pool *pgx.ConnPool
 
 func init() {
-	// os.Getenv("POSTGRES_URI")
+	conf, err := pgx.ParseConnectionString(os.Getenv("POSTGRES_URI"))
+	if err != nil {
+		panic(err)
+	}
+	Pool, err = pgx.NewConnPool(pgx.ConnPoolConfig{ConnConfig: conf})
+	if err != nil {
+		panic(err)
+	}
+
+	conn, err := Pool.Acquire()
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	err = conn.Ping(context.Background())
+	if err != nil {
+		panic(err)
+	}
 }
