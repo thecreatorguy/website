8632ab0b446d48fe0f71f266964a72e369edb95a
Tim Johnson <tim@itstimjohnson.com>
2020-11-07 14:29:25 -0500

    removed message sending  because it is spammed, need captcha

--- internal/app/message/message.go
+++ internal/app/message/message.go
@@ -2,40 +2,34 @@
 package message
 
 import (
-	"crypto/tls"
 	"net/http"
-	"net/smtp"
-	"os"
-	"website/internal/app/response"
-
-	"github.com/jordan-wright/email"
-	"github.com/sirupsen/logrus"
 )
 
 
 func sendMessageEndpoint(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-	if err != nil {
-		response.Write400(w)
-		return
-	}
+	// TODO: add this back in but with captcha so i don't get any spam anymore
+	// err := r.ParseForm()
+	// if err != nil {
+	// 	response.Write400(w)
+	// 	return
+	// }
 
-	e := email.NewEmail()
-	e.From = "Website Questions <questions.for.tim@itstimjohnson.com>"
-	e.To = []string{"tim@itstimjohnson.com"}
-	e.Cc = []string{r.Form.Get("email")}
-	e.Subject = "A Message From " + r.Form.Get("name")
-	e.Text = []byte(r.Form.Get("message"))
-	err = e.SendWithTLS(
-		os.Getenv("EMAIL_HOST") + ":" + os.Getenv("EMAIL_PORT"),
-		smtp.PlainAuth("", "questions.for.tim@itstimjohnson.com", os.Getenv("EMAIL_PASSWORD"), os.Getenv("EMAIL_HOST")),
-		&tls.Config{ServerName: os.Getenv("EMAIL_HOST")},
-	)
-	if err != nil {
-		logrus.WithError(err).Error("Failed sending email")
-		response.Write500(w)
-		return
-	}
+	// e := email.NewEmail()
+	// e.From = "Website Questions <questions.for.tim@itstimjohnson.com>"
+	// e.To = []string{"tim@itstimjohnson.com"}
+	// e.Cc = []string{r.Form.Get("email")}
+	// e.Subject = "A Message From " + r.Form.Get("name")
+	// e.Text = []byte(r.Form.Get("message"))
+	// err = e.SendWithTLS(
+	// 	os.Getenv("EMAIL_HOST") + ":" + os.Getenv("EMAIL_PORT"),
+	// 	smtp.PlainAuth("", "questions.for.tim@itstimjohnson.com", os.Getenv("EMAIL_PASSWORD"), os.Getenv("EMAIL_HOST")),
+	// 	&tls.Config{ServerName: os.Getenv("EMAIL_HOST")},
+	// )
+	// if err != nil {
+	// 	logrus.WithError(err).Error("Failed sending email")
+	// 	response.Write500(w)
+	// 	return
+	// }
 
 	http.Redirect(w, r, "/confirmation", http.StatusSeeOther)
 }
