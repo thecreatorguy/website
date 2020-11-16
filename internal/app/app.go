0dfabb19226019d50bcf5164fee5c3b1495800ea
Tim Johnson <tim@itstimjohnson.com>
2020-11-16 16:03:01 -0500

    removed unnecessary keypair reloader

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,10 +3,8 @@ package app
 
 import (
 	"crypto/tls"
-	"fmt"
 	"net/http"
 	"os"
-	"path/filepath"
 	"strings"
 	"time"
 	"website/internal/app/message"
@@ -44,29 +42,16 @@ func StartWebServer() {
 		MaxHeaderBytes: 1 << 20,
 	}
 	if os.Getenv("HTTPS") != "1" {
-		fmt.Println("there")
 		server.Addr = ":8675"
 		logrus.Info("Server ready to handle requests!")
 		logrus.Fatal(server.ListenAndServe())
 	} else {
-		var files []string
-		err := filepath.Walk("/certs", func(path string, info os.FileInfo, err error) error {
-			files = append(files, path)
-			return nil
-		})
+		cert, err := tls.LoadX509KeyPair(os.Getenv("SSL_CERT_PATH"), os.Getenv("SSL_KEYFILE_PATH"))
 		if err != nil {
-			panic(err)
-		}
-		fmt.Println(files)
-		kpr, err := NewKeypairReloader(os.Getenv("SSL_CERT_PATH"), os.Getenv("SSL_KEYFILE_PATH"))
-		if err != nil {
-			logrus.WithError(err).Fatal("Couldn't create key pair loader")
+			logrus.WithError(err).Fatal("Couldn't create ssl certificate")
 		}
 		server.Addr = ":8676"
-		server.TLSConfig = &tls.Config{
-			GetCertificate: kpr.GetCertificateFunc(),
-		}
-		fmt.Println("here")
+		server.TLSConfig = &tls.Config{Certificates: []tls.Certificate{cert}}
 		logrus.Info("Server ready to handle requests!")
 		logrus.Fatal(server.ListenAndServeTLS(os.Getenv("SSL_CERT_PATH"), os.Getenv("SSL_KEYFILE_PATH")))
 	}	
