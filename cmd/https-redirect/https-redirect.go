28ef49ce0d1878304d134113383640fc61a842e3
Tim Johnson <tim@itstimjohnson.com>
2020-11-16 21:40:50 -0500

    testing new redirect

--- cmd/https-redirect/https-redirect.go
+++ cmd/https-redirect/https-redirect.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -8,6 +9,7 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		r.URL.Scheme = "https"
+		fmt.Printf(r.URL.String())
 		http.Redirect(w, r, r.URL.String(), http.StatusPermanentRedirect)
 	})
 	log.Fatal(http.ListenAndServe(":80", nil))
