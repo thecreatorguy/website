150ca4a7257a4c538694c1b76017ac4192b9310b
Tim Johnson <tim@itstimjohnson.com>
2025-08-01 13:08:33 -0400

    fix renewcert

--- scripts/init_cert.sh
+++ scripts/init_cert.sh
@@ -3,6 +3,6 @@ hostnamectl set-hostname itstimjohnson.com
 
 sleep 5 # wait for the EIP to be assigned to this instance so the certbot can work
 certbot certonly --standalone --non-interactive --agree-tos -m tim@itstimjohnson.com -d itstimjohnson.com
-echo "0 0 1 */2 * /home/ubuntu/website/renewcert.sh" | crontab -
+echo "0 0 1 */2 * /home/ubuntu/website/scripts/renewcert.sh" | crontab -
 
 sudo -i -u ubuntu ./start_website.sh
--- scripts/renewcert.sh
+++ scripts/renewcert.sh
@@ -3,7 +3,7 @@
     echo "Renewing ssl certificate `date`"
     cd /home/ubuntu/website
     sudo make stopprod
-    certbot certonly --standalone --force-renew -d itstimjohnson.com
+    sudo certbot certonly --standalone --force-renew -d itstimjohnson.com
     sudo make runprod
     echo ""
 ) >> ~/cronlog 2>&1
