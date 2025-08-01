99de1592e1c8c51e1e15366e82bc399a21945d18
Tim Johnson <tim@itstimjohnson.com>
2025-08-01 13:07:25 -0400

    fix renewcert

--- scripts/renewcert.sh
+++ scripts/renewcert.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
-echo "Renewing ssl certificate `date`" >> ~/cronlog
-certbot certonly --standalone --force-renew -d itstimjohnson.com >> ~/cronlog
-echo "" >> ~/cronlog
+(
+    echo "Renewing ssl certificate `date`"
+    cd /home/ubuntu/website
+    sudo make stopprod
+    certbot certonly --standalone --force-renew -d itstimjohnson.com
+    sudo make runprod
+    echo ""
+) >> ~/cronlog 2>&1
