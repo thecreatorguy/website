67f89453c3a0cafbf1f9f447c9df7b3c1d33dca5
Tim Johnson <tim@itstimjohnson.com>
2025-08-01 12:47:23 -0400

    remove blog, contact

--- internal/app/page/routes.go
+++ internal/app/page/routes.go
@@ -5,8 +5,8 @@ import "github.com/gorilla/mux"
 // AddRoutes adds the page routes to the given router
 func AddRoutes(r *mux.Router) {
 	r.HandleFunc("/", renderHome).Methods("GET")
-	r.HandleFunc("/blog", renderBlog).Methods("GET")
+	// r.HandleFunc("/blog", renderBlog).Methods("GET")
 	r.HandleFunc("/{page}", renderPage).Methods("GET")
-	r.HandleFunc("/blog/articles/{article}", renderArticle).Methods("GET")
+	// r.HandleFunc("/blog/articles/{article}", renderArticle).Methods("GET")
 	r.HandleFunc("/projects/{project}", renderProject).Methods("GET")
 }
